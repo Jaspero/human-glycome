c682e3b22f9a4c9e40e3c84dc4f653de3c1eb50c
kristian <kristiantarnaj@gmail.com>
2026-01-30 09:55:05 +0100

    chore: use environment variables for firebase config

--- client2/src/lib/firebase.ts
+++ client2/src/lib/firebase.ts
@@ -1,14 +1,21 @@
 import {initializeApp} from 'firebase/app';
 import {getFirestore} from 'firebase/firestore';
+import {
+  PUBLIC_FIREBASE_API_KEY,
+  PUBLIC_FIREBASE_AUTH_DOMAIN,
+  PUBLIC_FIREBASE_PROJECT_ID,
+  PUBLIC_FIREBASE_STORAGE_BUCKET,
+  PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  PUBLIC_FIREBASE_APP_ID
+} from '$env/static/public';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBVK-OnAr3anNlpTYkXLFS_iaby24cv8II',
-  authDomain: 'human-glycome.firebaseapp.com',
-  databaseURL: 'https://human-glycome.firebaseio.com',
-  projectId: 'human-glycome',
-  storageBucket: 'human-glycome.appspot.com',
-  messagingSenderId: '233608642790',
-  appId: '1:233608642790:web:ffd8a2c8037cabce347b27'
+  apiKey: PUBLIC_FIREBASE_API_KEY,
+  authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
